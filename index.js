f83b96793e6a44f8e596c667d21aacc0ce3eb6a2
chantalnangole <chantalnafulacatsandkittens@gmail.com>
2026-03-31 08:47:34 +0300

    added drinks and burgers

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+let burgers = [ "Hamburgers", "Cheeseburger"]
+let featuredDrink = "StrawberryMilkshake"
+
+function addBurger() {
+  var newBurger = 'Flatburger';
+
+  burgers.push(newBurger);
+}
+
+if (true) {
+    let anotherNewBurger = "MapleBaconBurger"
+    burgers.push(anotherNewBurger)
+    
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "JavaShake"
+}
+
+console.log (burgers)
+console.log (featuredDrink)
